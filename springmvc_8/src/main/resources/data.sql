f8164c39576fe879385950fcb8956f7353f6e78e
ramyakollati <84019206+ramyakollati@users.noreply.github.com>
2021-05-15 11:25:21 +0530

    commit

---
+++ springmvc_8/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('ramya', 'Gupta', 'M G Road', 'hyderabad', '535128', 'India'),
+  	('John', 'Doe', 'City Road', 'Bangalore', '120340', 'India');
